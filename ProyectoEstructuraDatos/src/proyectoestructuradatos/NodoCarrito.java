91ae81b94019d6f635a2ade38e0a9ac46cb405e1
Ange3108 <aguatemala40593@ufide.ac.cr>
2025-08-06 21:02:23 -0600

    creacion de la clase libros

--- ProyectoEstructuraDatos/src/proyectoestructuradatos/NodoCarrito.java
+++ ProyectoEstructuraDatos/src/proyectoestructuradatos/NodoCarrito.java
@@ -1,6 +1,16 @@
 package proyectoestructuradatos;
 
+import javax.swing.JOptionPane;
+
 
 public class NodoCarrito {
-    
+     private Pedido pedido;
+    private NodoCarrito siguiente;
+
+    public NodoCarrito(Pedido pedido, NodoCarrito siguiente) {
+        this.pedido = pedido;
+        this.siguiente = null;
+    }
+
+
 }
