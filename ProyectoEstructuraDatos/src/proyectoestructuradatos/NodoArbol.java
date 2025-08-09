f927baae7d1853d23c590637956bc803b9665f43
Fabiola Urena <furena50122@ufide.ac.cr>
2025-08-08 23:29:45 -0600

    NodoArbol.java

--- ProyectoEstructuraDatos/src/proyectoestructuradatos/NodoArbol.java
+++ ProyectoEstructuraDatos/src/proyectoestructuradatos/NodoArbol.java
@@ -2,5 +2,38 @@ package proyectoestructuradatos;
 
 
 public class NodoArbol {
-    
+    private Libro elemento;
+    private NodoLibro enlaceIzq;
+    private NodoLibro enlaceDer;
+
+    public NodoLibro() {
+        this.enlaceIzq = null;
+        this.enlaceDer = null;
+    }
+
+    public Libro getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(Libro elemento) {
+        this.elemento = elemento;
+    }
+
+    public NodoLibro getEnlaceIzq() {
+        return enlaceIzq;
+    }
+
+    public void setEnlaceIzq(NodoLibro enlaceIzq) {
+        this.enlaceIzq = enlaceIzq;
+    }
+
+    public NodoLibro getEnlaceDer() {
+        return enlaceDer;
+    }
+
+    public void setEnlaceDer(NodoLibro enlaceDer) {
+        this.enlaceDer = enlaceDer;
+    }
+
 }
+
