85514fe3f36325ffb61e854f8035095526cec846
Ange3108 <aguatemala40593@ufide.ac.cr>
2025-08-12 21:36:47 -0600

    Lista Carrito, NodoCarrito y pedido

--- ProyectoEstructuraDatos/src/Modelo/Pedido.java
+++ ProyectoEstructuraDatos/src/Modelo/Pedido.java
@@ -1,54 +1,26 @@
 package Modelo;
+import Controlador.CtrlCatalogo;
+
 
 public class Pedido {
 
-    private int idPedido;
-    private Cliente nombre;
-    private Elemento elemento;
-    private Libro libro;
-    private Compra compra;
+    private CtrlCatalogo objeto;
+
 
     public Pedido() {
     }
 
-    public Pedido(int idPedido, Cliente nombre, Elemento elemento, Libro libro) {
-        this.idPedido = idPedido;
-        this.nombre = nombre;
-        this.elemento = elemento;
-        this.libro = libro;
+    public Pedido(CtrlCatalogo objeto) {
+
+        this.objeto = objeto;
     }
 
-    public int getIdPedido() {
-        return idPedido;
+
+    public CtrlCatalogo getObjeto() {
+        return objeto;
     }
 
-    public void setIdPedido(int idPedido) {
-        this.idPedido = idPedido;
+    public void setObjeto(CtrlCatalogo objeto) {
+        this.objeto = objeto;
     }
-
-    public Cliente getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(Cliente nombre) {
-        this.nombre = nombre;
-    }
-
-    public Elemento getElemento() {
-        return elemento;
-    }
-
-    public void setElemento(Elemento elemento) {
-        this.elemento = elemento;
-    }
-
-    public Libro getLibro() {
-        return libro;
-    }
-
-    public void setLibro(Libro libro) {
-        this.libro = libro;
-    }
-    
-    
 }
--- ProyectoEstructuraDatos/src/Nodos/NodoCarrito.java
+++ ProyectoEstructuraDatos/src/Nodos/NodoCarrito.java
@@ -1,16 +1,33 @@
 package Nodos;
 
 import Modelo.Pedido;
-import javax.swing.JOptionPane;
 
 public class NodoCarrito {
 
     private Pedido pedido;
     private NodoCarrito siguiente;
 
-    public NodoCarrito(Pedido pedido, NodoCarrito siguiente) {
+    public NodoCarrito(Pedido pedido) {
         this.pedido = pedido;
         this.siguiente = null;
     }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public NodoCarrito getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(NodoCarrito siguiente) {
+        this.siguiente = siguiente;
+    }
+
+
 
 }
